295743d28df690fc49a4ddb3efb2b2365322c831
Agostinho Sam'l <agostinhosaml832@gmail.com>
2021-12-13 13:12:21 +0100

    feat: create render method at src/pages/_document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,4 @@
-import Document from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class ApplicationDocument extends Document {
@@ -30,4 +30,20 @@ export default class ApplicationDocument extends Document {
       stylesheet.seal()
     }
   }
-}
+
+  render () {
+     return (
+       <Html>
+         <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;1,200;1,300;1,400&display=swap" rel="stylesheet" />
+         </Head>
+         <body>
+           <Main />
+           <NextScript />
+         </body>
+       </Html>
+     )
+  }
+ }
